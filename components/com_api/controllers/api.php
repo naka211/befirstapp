dd6624ae25c7f601d4c4f76c9a4a60142fb70f7f
T.Trung <nttrung211@yahoo.com>
2015-10-01 18:20:55 +0700

    dasd asd asd

--- components/com_api/controllers/api.php
+++ components/com_api/controllers/api.php
@@ -5,7 +5,32 @@ jimport('joomla.application.component.controller');
 
 class ApiControllerApi extends JControllerLegacy {
 
-    public function test() {
-        die('sffsdf');
+    public function login() {
+		$app    = JFactory::getApplication();
+		$username = JRequest::getVar("username");
+		$password = JRequest::getVar("password");
+		
+		$credentials["username"] = $username;
+		$credentials["password"] = $password;
+		
+		$options['remember'] = false;
+		$options['return']   = '';
+		
+		$result = $app->login($credentials, $options);
+		if($result == true){
+			//$result = 1;
+			$user = JFactory::getUser();
+			$userProfile = JUserHelper::getProfile( $user->id );
+			
+			$data = array("result" => 1,
+						"userid" => $user->id,
+						"name" => $user->name,
+						"email" => $user->email,
+						"gender" => $userProfile->profile["gender"]
+						
+			);
+			print_r();exit;
+		}
+        die(json_encode($data));
     }
 }
