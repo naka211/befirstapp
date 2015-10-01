f51bd5f6b05ebd55350f2fcfbc92489788af9dfd
Trung <nttrung211@yahoo.com>
2015-10-02 01:00:50 +0700

    dfsdf

--- components/com_api/controllers/api.php
+++ components/com_api/controllers/api.php
@@ -9,6 +9,8 @@ class ApiControllerApi extends JControllerLegacy {
 		$app    = JFactory::getApplication();
 		$username = JRequest::getVar("username");
 		$password = JRequest::getVar("password");
+		$type = JRequest::getVar("type");
+		$token = JRequest::getVar("token");
 		
 		$credentials["username"] = $username;
 		$credentials["password"] = $password;
@@ -18,18 +20,28 @@ class ApiControllerApi extends JControllerLegacy {
 		
 		$result = $app->login($credentials, $options);
 		if($result == true){
-			//$result = 1;
+			$db = JFactory::getDBO();
+			
 			$user = JFactory::getUser();
 			$userProfile = JUserHelper::getProfile( $user->id );
+			
+			$db->setQuery("INSERT INTO #__users_token (user_id, token, type) VALUES (".$user->id.", '".$token."', '".$type."')");
+			//$db->query();
 			
 			$data = array("result" => 1,
 						"userid" => $user->id,
 						"name" => $user->name,
 						"email" => $user->email,
-						"gender" => $userProfile->profile["gender"]
-						
+						"gender" => $userProfile->profile["gender"],
+						"dob" => JHtml::_('date', $userProfile->profile["dob"], 'd-m-Y'),
+						"address" => $userProfile->profile["address"],
+						"postal_code" => $userProfile->profile["postal_code"],
+						"city" => $userProfile->profile["city"],
+						"picture" => JURI::base()."media/plg_user_profilepicture/images/original/".$userProfile->profilepicture["file"]
 			);
-			print_r();exit;
+			//
+		} else {
+			$data = array("result" => 0);
 		}
         die(json_encode($data));
     }
